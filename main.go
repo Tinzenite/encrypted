487173bbb44ee9abbb795a19411fb99fc1dc353a
Tamino Hartmann <tamino.hartmann@gmail.com>
2015-09-11 13:26:29 +0200

    implemented first check

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package encrypted
 
+import (
+	"errors"
+
+	"github.com/tinzenite/shared"
+)
+
 /*
 Create returns a new Encrypted instance, ready to be connected to an existing
 network.
 */
-func Create() {}
+func Create(path, peerName string) (*Encrypted, error) {
+	if empty, _ := shared.IsDirectoryEmpty(path); !empty {
+		return nil, errors.New("non empty directory as path")
+	}
+	return nil, nil
+}
 
 /*
 Load returns the Encrypted structure for an existing instance.
